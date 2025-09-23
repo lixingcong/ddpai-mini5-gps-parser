61a17378f1d7ca88846465297d6f14fde21c74a2
lixingcong <lixingcong@live.com>
2025-09-23 13:18:53 +0800

    wip

--- src/components/OtherSettings.vue
+++ src/components/OtherSettings.vue
@@ -20,7 +20,7 @@ const urlAPISyncDate = serverHostUrl + import.meta.env.VITE_DDPAI_APISyncDate
 type HttpPostResolve = (content:string) => void
 type HttpPostReject = (content:Error) => void
 
-function promiseHttpPost(url:string, request:Request, withCredentials:boolean) {
+function promiseHttpPost(url:string, request:Request) {
 	return new Promise(function (resolve:HttpPostResolve, reject:HttpPostReject) {
 		let xhr = new XMLHttpRequest()
 		xhr.responseType = 'text'
@@ -31,7 +31,7 @@ function promiseHttpPost(url:string, request:Request, withCredentials:boolean) {
         for (const [key, value] of Object.entries(request.headers))
             xhr.setRequestHeader(key, value)
 
-        xhr.withCredentials = withCredentials // 跨域
+        xhr.withCredentials = true // 跨域
 
 		xhr.onreadystatechange = function () {
 			if (this.readyState === 4) {
@@ -54,17 +54,32 @@ function promiseHttpPost(url:string, request:Request, withCredentials:boolean) {
 const syncTime = () => {
     const apiRequestSessionID = new WEBAPI.RequestSessionID()
 
-    promiseHttpPost(urlAPIRequestSessionID, apiRequestSessionID.request(), true).then(
+    promiseHttpPost(urlAPIRequestSessionID, apiRequestSessionID.request()).then(
         (resolved) => {
             if(!apiRequestSessionID.parseResopnse(resolved))
                 return Promise.reject('RequestSessionID failed')
 
             const sessionId = apiRequestSessionID.sessionId
-            // console.log('sessionId', sessionId)
+            console.log('RequestSessionID ok, value=', sessionId)
             const apiRequestCertificate = new WEBAPI.RequestCertificate(sessionId)
 
-            return promiseHttpPost(urlAPIRequestCertificate, apiRequestCertificate.request(), true).then(
-                (resolved) => {console.log('RequestCertificate done')},
+            return promiseHttpPost(urlAPIRequestCertificate, apiRequestCertificate.request()).then(
+                (resolved) => {
+                    if(!apiRequestCertificate.parseResopnse(resolved))
+                        return Promise.reject('RequestCertificate failed')
+
+                    console.log('RequestCertificate done')
+
+                    const ts = Math.round((new Date()).getTime() / 1000)
+                    const apiSyncDate = new WEBAPI.SyncDate(sessionId, ts)
+
+                    return promiseHttpPost(urlAPISyncDate, apiSyncDate.request()).then(
+                        (resolved) => {
+                            console.log('SyncDate done')
+                        },
+                        (rejected) => onError
+                    )
+                },
                 (rejected) => onError
             )
         },
